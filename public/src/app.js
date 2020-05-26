8b3f98da246b042ef56b0d01b0b0664ad8f59fa5
Your Name <3767082@mail.ru>
2020-05-27 00:03:40 +0300

    optimizate mobile

--- public/src/app.js
+++ public/src/app.js
@@ -4,8 +4,11 @@ import './scss/typography.scss';
 import './scss/header.scss';
 import './scss/footer.scss';
 import './scss/skils-box.scss';
-import './scss/slider.scss';
 import './scss/about-me.scss';
+//sliders import
+import './scss/slider.scss';
+import './scss/slider-certificates.scss';
+import './scss/slider-project.scss';
 //js import
 import './js/rainbows.js';
 import './js/unicorns.js';
