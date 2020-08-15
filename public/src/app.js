948e8f059cfee86f0df3ab2a1c034f9a792e5fe5
Your Name <3767082@mail.ru>
2020-08-15 21:46:02 +0300

    add preloder

--- public/src/app.js
+++ public/src/app.js
@@ -5,17 +5,19 @@ import './scss/header.scss';
 import './scss/footer.scss';
 import './scss/skils-box.scss';
 import './scss/about-me.scss';
+import './scss/preloader.scss';
 //sliders import
 import './scss/slider.scss';
 import './scss/slider-certificates.scss';
 import './scss/slider-project.scss';
 //js import
+import  './js/lazy-loading.js';
 import './js/rainbows.js';
 import './js/unicorns.js';
 import './js/slider.js';
 import './js/dropmenu.js';
 import './js/scroll.js';
-import  './js/lazy-loading.js';
+
 
 
 
--- public/src/js/lazy-loading.js
+++ public/src/js/lazy-loading.js
@@ -1,19 +1,37 @@
+import * as $ from '../../node_modules/jquery';
 
-const promise = new Promise((resolve, reject) => {
-    resolve();
+// const promise = new Promise((resolve, reject) => {
+//     resolve();
+// });
+//
+// promise.then(function () {
+//     [].forEach.call(document.querySelectorAll('img[data-src]'), function (img) {
+//         img.setAttribute('src', img.getAttribute('data-src'));
+//         img.onload = function () {
+//             img.removeAttribute('data-src');
+//         };
+//     });
+//     [].forEach.call(document.querySelectorAll('img[data-src]'), function (img) {
+//         img.setAttribute('src', img.getAttribute('data-src'));
+//         img.onload = function () {
+//             img.removeAttribute('data-src');
+//         };
+//     });
+// });
+
+
+
+[].forEach.call(document.querySelectorAll('img[data-src]'), function (img) {
+
+    $('body').find('img').css('display', 'none');
+
+    img.setAttribute('src', img.getAttribute('data-src'));
+
+    $('body').find('.load-3').css('display', 'none');
+    $('body').find('img').css('display', 'block');
+
+    img.onload = function () {
+        img.removeAttribute('data-src');
+    };
 });
 
-promise.then(function () {
-    [].forEach.call(document.querySelectorAll('img[data-src]'), function (img) {
-        img.setAttribute('src', img.getAttribute('data-src'));
-        img.onload = function () {
-            img.removeAttribute('data-src');
-        };
-    });
-    [].forEach.call(document.querySelectorAll('img[data-src]'), function (img) {
-        img.setAttribute('src', img.getAttribute('data-src'));
-        img.onload = function () {
-            img.removeAttribute('data-src');
-        };
-    });
-});
