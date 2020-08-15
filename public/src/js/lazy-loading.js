bb4b54eaa5ab3038d3ee00d9265a1cef7ccadd22
Your Name <3767082@mail.ru>
2020-08-16 00:03:07 +0300

    added preloder fixed bug

--- public/src/js/lazy-loading.js
+++ public/src/js/lazy-loading.js
@@ -1,37 +1,29 @@
 import * as $ from '../../node_modules/jquery';
 
-// const promise = new Promise((resolve, reject) => {
-//     resolve();
-// });
-//
-// promise.then(function () {
-//     [].forEach.call(document.querySelectorAll('img[data-src]'), function (img) {
-//         img.setAttribute('src', img.getAttribute('data-src'));
-//         img.onload = function () {
-//             img.removeAttribute('data-src');
-//         };
-//     });
-//     [].forEach.call(document.querySelectorAll('img[data-src]'), function (img) {
-//         img.setAttribute('src', img.getAttribute('data-src'));
-//         img.onload = function () {
-//             img.removeAttribute('data-src');
-//         };
-//     });
-// });
+const line1= document.createElement('div');
+const line2 = document.createElement('div');
+const line3= document.createElement('div');
+line1.classList.add('line');
+line2.classList.add('line');
+line3.classList.add('line');
+const load3 = document.createElement('div');
+load3.classList.add('load-3');
+load3.appendChild(line1);
+load3.appendChild(line2);
+load3.appendChild(line3);
+$('header').find('picture').append(load3);
+$('body').find('picture').append(load3);
 
-
-
-[].forEach.call(document.querySelectorAll('img[data-src]'), function (img) {
-
-    $('body').find('img').css('display', 'none');
-
-    img.setAttribute('src', img.getAttribute('data-src'));
-
-    $('body').find('.load-3').css('display', 'none');
-    $('body').find('img').css('display', 'block');
-
-    img.onload = function () {
-        img.removeAttribute('data-src');
-    };
+const promise = new Promise((resolve, reject) => {
+    resolve();
 });
 
+promise.then(function () {
+    [].forEach.call(document.querySelectorAll('img[data-src]'), function (img) {
+        img.setAttribute('src', img.getAttribute('data-src'));
+        img.onload = function () {
+            img.removeAttribute('data-src');
+        };
+    });
+    $('body').find('.load-3').addClass('displayed');
+});
