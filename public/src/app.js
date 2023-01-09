1089da150a7f574329040dd7e5772a49b4f5a7fb
Aliaksei <aliaksei.zhorau@lindenvalley.de>
2023-01-09 17:10:43 +0300

    add cv

--- public/src/app.js
+++ public/src/app.js
@@ -6,10 +6,13 @@ import './scss/footer.scss';
 import './scss/skils-box.scss';
 import './scss/about-me.scss';
 import './scss/preloader.scss';
+import './scss/box-cv.scss';
+
 //sliders import
 import './scss/slider.scss';
 import './scss/slider-certificates.scss';
 import './scss/slider-project.scss';
+
 //js import
 import  './js/lazy-loading.js';
 import './js/rainbows.js';
@@ -17,6 +20,7 @@ import './js/unicorns.js';
 import './js/slider.js';
 import './js/dropmenu.js';
 import './js/scroll.js';
+import './js/print-pdf.js';
 
 
 
