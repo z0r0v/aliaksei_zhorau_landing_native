3c0e8e43268ff0d460df3c632dd8d6c20868cd78
Aliaksei <aliaksei.zhorau@lindenvalley.de>
2023-01-18 11:09:53 +0300

    remove code

--- public/src/app.js
+++ public/src/app.js
@@ -3,18 +3,6 @@ import './scss/base.scss';
 import './scss/typography.scss';
 import './scss/cv.scss';
 
-// import './scss/header.scss';
-// import './scss/footer.scss';
-// import './scss/skils-box.scss';
-// import './scss/about-me.scss';
-// import './scss/preloader.scss';
-
-
-//sliders import
-// import './scss/slider.scss';
-// import './scss/slider-certificates.scss';
-// import './scss/slider-project.scss';
-
 //js import
 import  './js/lazy-loading.js';
 import './js/print-pdf.js';
